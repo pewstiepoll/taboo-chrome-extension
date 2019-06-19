a2e06c4b1573c2498eea4a10c69c8e069b49c7aa
Vadym Zelenin <vzelenin@roku.com>
2019-06-19 18:40:28 +0300

    Modal I

--- src/index.js
+++ src/index.js
@@ -1,19 +1,57 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Shortcuts from "./components/shortcuts";
-import { Provider as ModalProvider } from "./components/modal/modalContext";
-import SimpleModal from "./components/modal/simple-modal";
 
 import "./styles.css";
 
+function useModal() {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  return {
+    isOpen,
+    openModal: () => setIsOpen(true),
+    closeModal: () => setIsOpen(false),
+    toggleModal: () => setIsOpen(({ isOpen }) => ({ isOpen: !isOpen }))
+  };
+}
+
+function Modal({ children, isOpen }) {
+  if (!isOpen) return null;
+
+  return ReactDOM.createPortal(
+    <>{children}</>,
+    document.getElementById("modal")
+  );
+}
+
+const SimpleModal = withModal(function SimpleModal(props) {
+  return <Modal {...props}>I'm am a simple modal</Modal>;
+});
+
+const ModalContext = React.createContext({});
+
+function withModal(Component) {
+  return props => (
+    <ModalContext.Consumer>
+      {context => <Component {...props} {...context} />}
+    </ModalContext.Consumer>
+  );
+}
+
+const ToggleModal = withModal(({ openModal }) => (
+  <button onClick={openModal}>ToggleModal</button>
+));
+
 function App() {
   return (
-    <ModalProvider>
+    <ModalContext.Provider value={useModal()}>
       <div className="App">
+        is modal open: {}
+        <ToggleModal />
         <Shortcuts />
         <SimpleModal />
       </div>
-    </ModalProvider>
+    </ModalContext.Provider>
   );
 }
 

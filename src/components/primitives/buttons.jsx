c71c6e3453a5b510ccf1a49f950f11ff0acaed7f
Vadym <wadedevelop@gmail.com>
2019-06-24 23:01:39 +0300

    More <Button /> styling

--- src/components/primitives/buttons.jsx
+++ src/components/primitives/buttons.jsx
@@ -3,16 +3,22 @@ import PropTypes from "prop-types";
 
 import styles from "./buttons.module.css";
 
-export function Button({ styleType, ...props }) {
-  const classes = [styles.button, styles[`button--${styleType}`]];
+export function Button({ styleType, size, ...props }) {
+  const classes = [
+    styles.button,
+    styles[`button--${styleType}`],
+    styles[`button--${size}`]
+  ];
 
   return <button className={classes.join(" ")} {...props} />;
 }
 
 Button.defaultProps = {
-  styleType: "primary"
+  styleType: "primary",
+  size: "standard"
 };
 
 Button.propTypes = {
-  styleType: PropTypes.oneOf(["primary", "notice", "danger"])
+  styleType: PropTypes.oneOf(["primary", "notice", "danger"]),
+  size: PropTypes.oneOf(["short", "standard", "long"])
 };

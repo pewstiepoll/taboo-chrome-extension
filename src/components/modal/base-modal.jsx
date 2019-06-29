415993b9f415e92416fecf6068f748a0025c5869
Vadym <wadedevelop@gmail.com>
2019-06-29 13:01:49 +0300

    Base modal redesign

--- src/components/modal/base-modal.jsx
+++ src/components/modal/base-modal.jsx
@@ -1,19 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withModal, Modal } from "./index";
-import { Button } from "../primitives";
+import { Button, Stack, Box, Cluster } from "../primitives";
+import { formDataToObject } from "../../utils";
 
 import styles from "./base-modal.module.css";
-
-function formDataToObject(formData) {
-  const result = {};
-
-  for (let [key, value] of formData.entries()) {
-    result[key] = value;
-  }
-
-  return result;
-}
 
 const createFields = fields => () => {
   const refs = [];
@@ -23,7 +14,7 @@ const createFields = fields => () => {
   }, [refs]);
 
   return fields.map(fieldProps => (
-    <div className={styles["field"]} key={fieldProps.name}>
+    <Stack reversed className={styles["field"]} key={fieldProps.name}>
       <input
         className={styles["field-input"]}
         ref={input => {
@@ -35,7 +26,7 @@ const createFields = fields => () => {
       <label className={styles["field-label"]} htmlFor={fieldProps.name}>
         {fieldProps.label || fieldProps.placeholder || fieldProps.name}
       </label>
-    </div>
+    </Stack>
   ));
 };
 
@@ -43,43 +34,88 @@ const createButtons = buttons => () => {
   return buttons.map((buttonProps, id) => <Button key={id} {...buttonProps} />);
 };
 
-const BaseModal = withModal(function BaseModal({ modal, closeModal }) {
-  const { fields = [], buttons = [] } = modal.params;
+/**
+ * Base modal form components
+ */
+export const BaseModalHeader = ({ title }) => (
+  <h1 className={styles["modal-header"]}>{title}</h1>
+);
+
+export const BaseModalContent = ({ fields = [] }) => {
+  if (!fields.length) return null;
 
   const Fields = createFields(fields);
+
+  return (
+    <div className="modal-content">
+      <Fields />
+    </div>
+  );
+};
+
+export const BaseModalFooter = ({ buttons = [] }) => {
+  if (!buttons.length) return null;
+
   const Buttons = createButtons(buttons);
 
   return (
+    <Cluster align="right" className={styles["modal-footer"]}>
+      <Buttons />
+    </Cluster>
+  );
+};
+
+/**
+ * Base modal form composition
+ */
+export const BaseModalForm = withModal(({ modal, closeModal }) => {
+  const { fields = [], buttons = [] } = modal.params;
+
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+
+        // Collect form data from the DOM
+        const result = formDataToObject(new FormData(e.target));
+
+        // Send form data up to the parent
+        if (typeof modal.params.onSubmit === "function")
+          modal.params.onSubmit(result);
+
+        // Close modal
+        closeModal();
+      }}
+      className={styles["modal-form"]}
+    >
+      <Stack>
+        <BaseModalHeader title={modal.params.title} />
+        <BaseModalContent fields={fields} />
+        <BaseModalFooter buttons={buttons} />
+      </Stack>
+    </form>
+  );
+});
+
+export const BaseModalContainer = () => (
+  <Box
+    alignment={{ horizontal: "center", vertical: "center" }}
+    onClick={e => e.stopPropagation()}
+    className={styles["modal-container"]}
+  >
+    <BaseModalForm />
+  </Box>
+);
+
+/**
+ * Base modal component
+ */
+const BaseModal = withModal(function BaseModal({ modal, closeModal }) {
+  return (
     <Modal {...modal}>
-      <div onClick={closeModal} className={styles["modal-overlay"]}>
-        <div
-          onClick={e => e.stopPropagation()}
-          className={styles["modal-container"]}
-        >
-          <h1 className={styles["modal-title"]}>{modal.params.title}</h1>
-          <form
-            onSubmit={e => {
-              e.preventDefault();
-
-              // Collect form data from the DOM
-              const result = formDataToObject(new FormData(e.target));
-
-              // Send form data up to the parent
-              if (typeof modal.params.onSubmit === "function")
-                modal.params.onSubmit(result);
-
-              // Close modal
-              closeModal();
-            }}
-            className={styles["modal-form"]}
-          >
-            <Fields />
-            <div className={styles["modal-footer"]}>
-              <Buttons />
-            </div>
-          </form>
-        </div>
-      </div>
+      <Box onClick={closeModal} className={styles["modal-overlay"]}>
+        <BaseModalContainer />
+      </Box>
     </Modal>
   );
 });

91ef065d079a88ed7d157b04755eefd30f92122d
Vadym <wadedevelop@gmail.com>
2019-06-29 13:01:22 +0300

    Added <Cluster />

---
+++ src/components/primitives/cluster.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import styles from "./cluster.module.css";
+import { mergeClassNames } from "../../utils";
+/**
+ * Cluster is a block component for displaying and aligning inline children
+ */
+
+export function getClusterChildrenAlignmentClasses(align) {
+  return [styles["cluster"], styles[`cluster-align--${align}`]];
+}
+
+export function Cluster({ align, className: classNameProp, ...props }) {
+  const clusterClasses = mergeClassNames(
+    classNameProp,
+    getClusterChildrenAlignmentClasses(align)
+  );
+  return <div className={clusterClasses} {...props} />;
+}

dc893745a73b5ec46ad256703c1cbc1c5ac830f5
Vadym <wadedevelop@gmail.com>
2019-06-29 13:00:42 +0300

    New helpers

--- src/utils.jsx
+++ src/utils.jsx
@@ -3,3 +3,28 @@ export function bindActionCreator(dispatch, action) {
     dispatch(action(...params));
   };
 }
+
+export function mergeClassNames(...classNames) {
+  return classNames
+    .map(className => {
+      if (!className) return "";
+
+      if (typeof className === "string") return className;
+      if (Array.isArray(className)) return className.join(" ");
+
+      throw TypeError(
+        "mergeClassNames helper can only take strings and arrays as values."
+      );
+    })
+    .join(" ");
+}
+
+export function formDataToObject(formData) {
+  const result = {};
+
+  for (let [key, value] of formData.entries()) {
+    result[key] = value;
+  }
+
+  return result;
+}
